ccaf74fcf6edcb1b7ad53adf416b6288f7d14781
amatra-m <amatramenon7@gmail.com>
2026-03-03 00:04:14 +0530

    UC3 - Refactored banner using String.join()

--- src/OOPsBannerApp.java
+++ src/OOPsBannerApp.java
@@ -1,18 +1,19 @@
 /**
- * OOPSBannerApp UC2 - Banner Display Application
+ * OOPSBannerApp UC3 - OOPS Banner Application
+ * Using String.join() instead of + for concatenation
  */
 
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("  *****   " + "  *****   " + "  ******  " + "  ******  ");
-        System.out.println(" *     *  " + " *     *  " + " *     *  " + " *        ");
-        System.out.println(" *     *  " + " *     *  " + " *     *  " + " *        ");
-        System.out.println(" *     *  " + " *     *  " + " ******   " + "  *****   ");
-        System.out.println(" *     *  " + " *     *  " + " *        " + "       *  ");
-        System.out.println(" *     *  " + " *     *  " + " *        " + "       *  ");
-        System.out.println("  *****   " + "  *****   " + " *        " + "  *****   ");
+        System.out.println(String.join("", "  *****   ", "  *****   ", "  ******  ", "  ******  "));
+        System.out.println(String.join("", " *     *  ", " *     *  ", " *     *  ", " *        "));
+        System.out.println(String.join("", " *     *  ", " *     *  ", " *     *  ", " *        "));
+        System.out.println(String.join("", " *     *  ", " *     *  ", " ******   ", "  *****   "));
+        System.out.println(String.join("", " *     *  ", " *     *  ", " *        ", "       *  "));
+        System.out.println(String.join("", " *     *  ", " *     *  ", " *        ", "       *  "));
+        System.out.println(String.join("", "  *****   ", "  *****   ", " *        ", "  *****   "));
 
     }
 }

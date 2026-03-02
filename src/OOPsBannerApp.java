3e6be27e99843537d9ec769ea8c5aa1d4f3e17bd
amatra-m <amatramenon7@gmail.com>
2026-03-03 00:07:19 +0530

    UC3 - Refactored banner using String.join()

--- src/OOPsBannerApp.java
+++ src/OOPsBannerApp.java
@@ -1,19 +1,23 @@
 /**
- * OOPSBannerApp UC3 - OOPS Banner Application
- * Using String.join() instead of + for concatenation
+ * OOPSBannerApp UC4 - Render OOPS as Banner using String Array and Loop
  */
 
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(String.join("", "  *****   ", "  *****   ", "  ******  ", "  ******  "));
-        System.out.println(String.join("", " *     *  ", " *     *  ", " *     *  ", " *        "));
-        System.out.println(String.join("", " *     *  ", " *     *  ", " *     *  ", " *        "));
-        System.out.println(String.join("", " *     *  ", " *     *  ", " ******   ", "  *****   "));
-        System.out.println(String.join("", " *     *  ", " *     *  ", " *        ", "       *  "));
-        System.out.println(String.join("", " *     *  ", " *     *  ", " *        ", "       *  "));
-        System.out.println(String.join("", "  *****   ", "  *****   ", " *        ", "  *****   "));
+        String[] lines = new String[7];
 
+        lines[0] = String.join("", "  *****   ", "  *****   ", "  ******  ", "  ******  ");
+        lines[1] = String.join("", " *     *  ", " *     *  ", " *     *  ", " *        ");
+        lines[2] = String.join("", " *     *  ", " *     *  ", " *     *  ", " *        ");
+        lines[3] = String.join("", " *     *  ", " *     *  ", " ******   ", "  *****   ");
+        lines[4] = String.join("", " *     *  ", " *     *  ", " *        ", "       *  ");
+        lines[5] = String.join("", " *     *  ", " *     *  ", " *        ", "       *  ");
+        lines[6] = String.join("", "  *****   ", "  *****   ", " *        ", "  *****   ");
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
     }
 }

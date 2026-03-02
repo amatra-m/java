6858acdf0186296af5ca4d39e014beb3a16a459f
amatra-m <amatramenon7@gmail.com>
2026-03-03 00:00:42 +0530

    UC2 - Display OOPS banner

--- src/OOPsBannerApp.java
+++ src/OOPsBannerApp.java
@@ -1,7 +1,18 @@
-public class OOPsBannerApp {
+/**
+ * OOPSBannerApp UC2 - Banner Display Application
+ */
 
-    public static void main(String args[]){
-        System.out.println("Welcome");
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("  *****   " + "  *****   " + "  ******  " + "  ******  ");
+        System.out.println(" *     *  " + " *     *  " + " *     *  " + " *        ");
+        System.out.println(" *     *  " + " *     *  " + " *     *  " + " *        ");
+        System.out.println(" *     *  " + " *     *  " + " ******   " + "  *****   ");
+        System.out.println(" *     *  " + " *     *  " + " *        " + "       *  ");
+        System.out.println(" *     *  " + " *     *  " + " *        " + "       *  ");
+        System.out.println("  *****   " + "  *****   " + " *        " + "  *****   ");
+
     }
-
 }

830cf5606b2108f3b7900e5454780f7da8f61250
amatra-m <amatramenon7@gmail.com>
2026-03-11 08:49:43 +0530

    Coded UC1: Display Hello World in console

---
+++ src/com/oopsbanner/HelloApp.java
@@ -0,0 +1,13 @@
+package com.oopsbanner;
+
+/**
+ * UC1: Display Hello World
+ */
+
+public class HelloApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
